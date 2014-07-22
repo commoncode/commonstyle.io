b222b8c07062beb64b2bb666fd514c7f09d5cc33
Darren Frenkel <darrenfrenkel@gmail.com>
2014-07-22 13:25:30 +1000

    added fab and roles.py file

---
+++ app/roles.py
@@ -0,0 +1,7 @@
+vhost = {
+	'commonstyle.io': {
+		'hosts': ['ip-10-0-5-86.ap-southeast-2.compute.internal'],
+		'gateway': 'meteorhosts-gw-ap-southeast-2a.commoncode.com.au',
+		'flow_api_token': '705f3a4015251965aeb9180e5cc0ec42'
+	}
+}
